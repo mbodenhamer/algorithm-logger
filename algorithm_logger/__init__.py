a4fdc0ed30cd49481f5b65c037592bd83b53d02d
mbodenhamer <mbodenhamer@mbodenhamer.com>
2018-06-22 23:57:35 -0500

    Adding spec.py and logger.py

--- algorithm_logger/__init__.py
+++ algorithm_logger/__init__.py
@@ -1,1 +1,3 @@
 from .base import *
+from .spec import *
+from .logger import *
---
+++ algorithm_logger/tests/test_logger.py
@@ -0,0 +1,13 @@
+from algorithm_logger import Logger
+
+#-------------------------------------------------------------------------------
+# Logger
+
+def test_logger():
+    Logger
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__': # pragma: no cover
+    from syn.base_utils import run_all_tests
+    run_all_tests(globals(), verbose=True, print_errors=False)
